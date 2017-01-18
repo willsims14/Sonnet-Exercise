2079cc0a7cf85220c3b913d3d224024eae00ec85
William A Sims III <willsims14@gmail.com>
2017-01-18 12:07:59 -0600

    Moved code around

--- sonnet.js
+++ sonnet.js
@@ -1,18 +1,18 @@
+// Get HTML element
 var sonnetDiv = document.getElementById("sonnet").textContent;
+
+// Variables
 var newSonnet1;
 var newSonnet2;
 
-console.log("Sonnet: \n", sonnetDiv);
-
-
-console.log("Strating Position of the word 'orphans': ", sonnetDiv.search("orphans"));
-console.log("Number of Characters in Sonnet: ",sonnetDiv.length);
-
-// Replace 'winter' with 'yuletide'
+// Replace words in string
 newSonnet1 = sonnetDiv.replace("winter", "yuletide");
-console.log("New Sonnet: \n", newSonnet1);
-
-
-// Replace 'the' with 'a large'
 newSonnet2 = newSonnet1.replace(" the ", " a large ");
+
+
+// Log new sonnets
 console.log("New Sonnet2: \n", newSonnet2);
+console.log("New Sonnet: \n", newSonnet1);
+console.log("Number of Characters in Sonnet: ",sonnetDiv.length);
+console.log("Sonnet: \n", sonnetDiv);
+console.log("Strating Position of the word 'orphans': ", sonnetDiv.search("orphans"));
